94432f08f86414674581e5e0860a7acdac41bf9c
Alan Christianson <git@c10n.net>
2017-10-29 06:56:01 -0600

    Add keys command

--- rau.py
+++ rau.py
@@ -2,29 +2,46 @@ import argparse
 from redis import StrictRedis
 from rau.commands import Command
 
+
 def delete(args, command):
     """ Execute the delete command """
     command.delete(args.pattern)
+
+
+def keys(args, command):
+    """ Execute the keys command """
+    command.keys(args.pattern, args.details)
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-H', '--host', default='localhost', help='Host')
     parser.add_argument('-p', '--port', default=6379, type=int, help='Port')
+
     subparsers = parser.add_subparsers(help='Commands')
-    del_parser = subparsers.add_parser('delete',
-                                        help='Delete key(s)')
+    del_parser = subparsers.add_parser('delete', help='Delete key(s)')
     del_parser.add_argument('pattern', type=str, help='Key pattern')
     del_parser.set_defaults(func=delete)
+
+    key_parser = subparsers.add_parser('keys', help="List keys")
+    key_parser.add_argument('-p', '--pattern', help='Key pattern',
+                            default=None)
+    key_parser.add_argument('-d', '--details',
+                            help='Include details for key(s)',
+                            action='store_true')
+    key_parser.set_defaults(func=keys)
 
     args = parser.parse_args()
     print args
     return args
+
 
 def main():
     args = parse_args()
     redis = StrictRedis(host=args.host, port=args.port)
     command = Command(redis)
     args.func(args, command)
+
 
 if __name__ == '__main__':
     main()
--- rau/commands.py
+++ rau/commands.py
@@ -1,4 +1,7 @@
+import humanfriendly
 from redis import StrictRedis
+
+BATCH_SIZE = 20
 
 
 class Command:
@@ -9,9 +12,42 @@ class Command:
         else:
             self.redis = redis_obj
 
+    def _get_obj_details(self, obj):
+        """ Given debug data, extract values of interest """
+        return obj['serializedlength']
+
     def delete(self, key_pattern):
         """ Delete keys matching the specified pattern """
-        print("Deleting %s" % key_pattern)
         for key in self.redis.scan_iter(key_pattern):
-            print("Found key: %s" % key)
+            print("Deleting key: %s" % key)
             self.redis.delete(key)
+
+    def get_details(self, key_list):
+        """ Get debug info for specified keys """
+        pipe = self.redis.pipeline()
+        details = []
+
+        strides = xrange(0, len(key_list), BATCH_SIZE)
+        for i in strides:
+            for j in key_list[i: i + BATCH_SIZE]:
+                pipe.debug_object(j)
+            batch_dets = pipe.execute()
+            details.extend(map(self._get_obj_details, batch_dets))
+
+        return zip(key_list, details)
+
+    def keys(self, key_pattern, details=False):
+        """ List keys matching pattern, optionally with details like size """
+        key_list = list(self.redis.scan_iter(key_pattern))
+
+        if details:
+            detailed_result = self.get_details(key_list)
+            result = []
+            for r in detailed_result:
+                result.append('%s : %s' % (r[0],
+                              humanfriendly.format_size(r[1])))
+        else:
+            result = key_list
+
+        for value in result:
+            print(value)
